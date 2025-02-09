2c18a08c6645349c6b146450c6f501825f5ecbe9
lesliehuh <57726465+lesliehuh@users.noreply.github.com>
2025-02-09 19:52:48 +0000

    syntax fixing

--- src/user/anon.js
+++ src/user/anon.js
@@ -8,34 +8,34 @@ function getRandomInt(min, max) {
   }
 
 module.exports = function (User) {
-    // function that creates secondary anonymous account
-    User.anonCreate = async function (data) {
-      const tempUser = data.username.concat(getRandomInt(100000,999999));
-      const anonAcc = {         // anon account template
-        uid: 0,
-        username: tempUser,
-        displayname: 'Anonymous',
-        userslug: '',
-        fullname: 'Anonymous',
-        email: data.email,
-        'icon:text': '?',
-        'icon:bgColor': '#aaa',
-        groupTitle: '',
-        groupTitleArray: [],
-        status: 'offline',
-        reputation: 0,
-        'email:confirmed': data['email:confirmed'],
-      };
-      data.anonUID = tempUser;
-      try {
-        // Create the anonymous account
-        await User.create(anonAcc);
-        console.log("Anonymous account created with UID:", tempUser);
-      } catch (error) {
-        console.error("Error creating anonymous account:", error);
-        throw error; // Or handle as needed
-      }
+  // function that creates secondary anonymous account
+  User.anonCreate = async function (data) {
+    const tempUser = data.username.concat(getRandomInt(100000,999999));
+    const anonAcc = {         // anon account template
+      uid: 0,
+      username: tempUser,
+      displayname: 'Anonymous',
+      userslug: '',
+      fullname: 'Anonymous',
+      email: data.email,
+      'icon:text': '?',
+      'icon:bgColor': '#aaa',
+      groupTitle: '',
+      groupTitleArray: [],
+      status: 'offline',
+      reputation: 0,
+      'email:confirmed': data['email:confirmed'],
+    };
+    data.anonUID = tempUser;
+    try {
+      // Create the anonymous account
+      await User.create(anonAcc);
+      console.log("Anonymous account created with UID:", tempUser);
+    } catch (error) {
+      console.error("Error creating anonymous account:", error);
+      throw error; // Or handle as needed
     }
-    // function that creates group dedicated to just anonymous accounts
-    
+  }
+  // function that creates group dedicated to just anonymous accounts
+  
 }

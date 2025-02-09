a54d36f6517c02afe8905b6fe31a969f7b3921f3
lesliehuh <57726465+lesliehuh@users.noreply.github.com>
2025-02-09 19:20:01 +0000

    added 'use strict' to use strict mode

--- src/user/anon.js
+++ src/user/anon.js
@@ -1,3 +1,4 @@
+'use strict';
 // random number generating function for creating anon uid
 // sourced from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {

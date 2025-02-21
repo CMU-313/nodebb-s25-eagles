a05e4a469c666d281fd73e5b5cd056d70cd39d0c
lesliehuh <57726465+lesliehuh@users.noreply.github.com>
2025-02-21 16:01:44 +0000

    syntax error fix number 2304982405968340958

--- test/user.js
+++ test/user.js
@@ -923,7 +923,7 @@ describe('User', () => {
 				assert.ifError(err);
 				assert.equal(data.uid, uid);
 				assert.equal(data.status, 'anonymous');
-				console.log(user.getUserField(uid,'anonymous'));
+				console.log(User.getUserField(uid,'anonymous'));
 				// assert.equal(data.anonymous, true);
 				done();
 			});

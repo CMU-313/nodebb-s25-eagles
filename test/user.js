7eea98dd163da78f4ae7b1ae5a57b9c0e3240e9b
lesliehuh <57726465+lesliehuh@users.noreply.github.com>
2025-02-21 16:16:57 +0000

    trying assert.strictEqual

--- test/user.js
+++ test/user.js
@@ -923,8 +923,7 @@ describe('User', () => {
 				assert.ifError(err);
 				assert.equal(data.uid, uid);
 				assert.equal(data.status, 'anonymous');
-				console.log(User.getUserField(uid,'anonymous'));
-				console.log(User.getStatus(uid))
+				assert.strictEqual (data.anonymous, '1')
 				// assert.equal(data.anonymous, true);
 				done();
 			});

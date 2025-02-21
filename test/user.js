ee913ee4a7b81b9f8254f30f8634a63f9b5bb692
lesliehuh <57726465+lesliehuh@users.noreply.github.com>
2025-02-21 16:53:07 +0000

    small change for testing data.anonymous (again)

--- test/user.js
+++ test/user.js
@@ -927,7 +927,7 @@ describe('User', () => {
 				assert.ifError(err);
 				assert.equal(data.uid, uid);
 				assert.equal(data.status, 'anonymous');
-				assert.strictEqual (User.getUserField(uid, 'anonymous'), '1')
+				assert.equal (User.getUserField(uid, 'anonymous'), '1')
 				// assert.equal(data.anonymous, true);
 				done();
 			});

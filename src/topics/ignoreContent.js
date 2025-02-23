839e77a2767480f32c66b0a4465b45e0a8e07410
VallinZ <107499985+VallinZ@users.noreply.github.com>
2025-02-23 13:57:44 -0500

    Merge pull request #38 from CMU-313/ChatBotFinal
    Auto-Reply ChatBot -- Create a bot account and make Deepseek API Call

---
+++ src/topics/ignoreContent.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const testContents = ['Test topic content', 'Test topic 2 content', 'Test topic 3 content',
+	'The content of test topic', 'test topic content', 'some content', 'topic 1 OP', 'topic 2 OP', 'topic 3 OP',
+	'does not really matter', 'This is flaggable content', 'private post', '123456789', 'xxxxxxxx', 'original post',
+	'A post to delete/restore/purge', 'test topic', 'A post to edit', 'Some text here for the OP',
+	'avocado cucumber apple orange fox', 'avocado cucumber carrot armadillo', "This is the test topic's content",
+	'The content of deleted topic', 'New post content', 'Test topic 2 edited content', 'test post',
+	'a topic with noscript', 'private post12', 'queued topic', 'queued topic content', 'topic content', 'topic A content',
+	'topic B content', 'content for the main post', 'main post', 'topic 1 mainPost', 'topic 2 mainpost',
+	'Topic to be ignored', 'topic title 1', 'topic title 2', 'topic title 3', 'unread topic',
+	'topic in private category', 'not unread', 'topic 1 content', 'topic 2 content', 'topic 3 content', 'Just ignore me, please!',
+	'unread topic content', 'registered-users cant see this', 'content 1', 'content 2', 'some content here',
+	'The content of scheduled test topic', 'lorem ipsum', 'the content', 'old user topic post content', 'tttttttttttt',
+];
+
+module.exports = testContents;

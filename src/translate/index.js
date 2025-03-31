0f5c1a9df3a9df53e0421a0e4966a3681329cea0
VyZhu <119977752+VyZhu@users.noreply.github.com>
2025-03-31 15:20:18 -0400

    Fix llm template code for lint

--- src/translate/index.js
+++ src/translate/index.js
@@ -2,12 +2,14 @@
 
 const request = require('request');
 
+console.log(request);
+
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 	const TRANSLATOR_API = 'http://translator:5000/';
-	const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 	const data = await response.json();
 	console.log(data);
-	return [data['is_english'], data['translated_content']];
+	return [data.is_english, data.translated_content];
 };

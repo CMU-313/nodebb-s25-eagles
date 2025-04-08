cd80294f956bfc9faf1b3a981b09dbad3b0764e9
VallinZ <107499985+VallinZ@users.noreply.github.com>
2025-04-08 17:00:40 -0400

    Revert "Add some console.log to debug translation api"

--- src/translate/index.js
+++ src/translate/index.js
@@ -7,13 +7,11 @@ translatorApi.translate = async function (postData) {
 	try {
 		const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 		const data = await response.json();
-		console.log(data);
 		if (!data || data.is_english === undefined || data.translated_content === undefined) {
 			return [true, postData.content];
 		}
 		return [data.is_english, data.translated_content];
 	} catch (someError) {
-		console.error(someError);
 		return [true, postData.content];
 	}
 };
